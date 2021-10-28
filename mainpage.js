b8ee27b6512fb2e5890b6bb6b9991bebeace2384
akashiseijurou <84743328+akashiseijurou@users.noreply.github.com>
2021-10-27 17:59:29 -0700

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,31 @@
+const cd = document.getElementById('cd');
+const tts = document.getElementById('tts');
+const relaxer = document.getElementById('relaxer');
+const mplayer = document.getElementById('mplayer');
+
+// functions
+
+
+
+/*
+//event listeners
+cd.addEventListener('click', () => {
+    console.log("work");
+    window.location.href = 'C:\Users\sena\Desktop\test\mainpage\cd.html';
+});
+
+/*
+tts.addEventListener('click', toTts);
+relaxer.addEventListener('click', toRelaxer);
+mplayer.addEventListener('click', toMplayer);
+
+
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+*/
